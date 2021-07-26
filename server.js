99f3b96c3d9e2d502f4d24783071349369b03b35
Greg <gskril@gmail.com>
2021-07-26 18:14:36 -0400

    Add electron-builder

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const main = require('./main')
 const port = main.getAvailPort
 const scrape = require('./app')
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 app.listen(port)
 
 console.log(`Server is listening on port ${port} \n- http://localhost:${port}/`);
