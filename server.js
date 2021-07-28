4797d7f342f058dbcee4fd429e4f841014988a70
Greg <gskril@gmail.com>
2021-07-28 15:05:21 -0400

    Disable input when function is running

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/', async (req, res) => {
 	})
 })
 
+app.get('/scraper', async (req, res) => {
+	res.redirect('/')
+})
+
 app.post('/scraper', async (req, res) => {
 	scrape.scrape(req.body.username, res)
 })
